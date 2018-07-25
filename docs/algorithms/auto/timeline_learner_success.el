c68fe1df5d606aff70f96f6f24f97472da98208b
FeLungs <will@yetanalytics.com>
2018-07-24 21:12:24 -0400

    updated z schemas for properties of a statement so that I can use JSON path notation within Operation schemas

---
+++ docs/algorithms/auto/timeline_learner_success.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "timeline_learner_success"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm2e" "ruled")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "algorithm2e"
+    "hyperref"
+    "amsmath"
+    "zed-csp"
+    "breqn"
+    "listings")
+   (LaTeX-add-labels
+    "moreLink"
+    "noZ"))
+ :latex)
+
