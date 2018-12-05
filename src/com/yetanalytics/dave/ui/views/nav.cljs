390fbe1152fff9f1e9b130daede153f21e58707d
Milt Reder <milt@yetanalytics.com>
2018-12-05 13:22:05 -0500

    breadcrumbs work for actual navigation

--- src/com/yetanalytics/dave/ui/views/nav.cljs
+++ src/com/yetanalytics/dave/ui/views/nav.cljs
@@ -1,40 +1,62 @@
 (ns com.yetanalytics.dave.ui.views.nav
   "Header, footer and breadcrumb nav components"
   (:require [re-frame.core :refer [dispatch subscribe]]
-
+            [clojure.string :as cs]
+            [goog.string :refer [format]]
+            [goog.string.format]
             ;; TODO: remove
             [cljs.pprint :refer [pprint]]
             ))
 
-(defn crumb [title text active?]
+(defn crumb [{:keys [title text active?
+                     href]}]
   [:div
    {:class (when active? "active")}
-   [:div.title title]
-   [:div.text text]])
+   [:a {:href (when href
+                href)}
+
+    [:div.title title]
+    [:div.text text]]])
 
 (defn breadcrumbs
   "Based on context/path, display the DAVE breadcrumb nav to the user."
   []
-  (let [context @(subscribe [:nav/context])]
+  (let [context @(subscribe [:nav/context])
+        [?workbook
+         ?question
+         ?visualization] @(subscribe [:nav/path-items])]
     [:div.breadcrumbs
      [:div ;; inner
       [crumb
-       "DAVE"
-       "Some text about the workbook list"
-       (= :root context)]
+       {:title "DAVE"
+        :text "Some text about the workbook list"
+        :active? (= :root context)
+        :href "#/"}]
       [crumb
-       "Workbook"
-       "Some text about workbooks"
-       (= :workbook context)]
+       {:title "Workbook"
+        :text "Some text about workbooks"
+        :active? (= :workbook context)
+        :href (when ?workbook
+                (format "#/workbooks/%s" (:id ?workbook)))}
+       ]
       [crumb
-       "Question"
-       "Some text about questions"
-       (= :question context)]
+       {:title "Question"
+        :text "Some text about questions"
+        :active? (= :question context)
+        :href (when ?question
+                ;; We know that if a question is there, a workbook is too
+                (format "#/workbooks/%s/questions/%s"
+                        (:id ?workbook)
+                        (:id ?question)))}]
       [crumb
-       "Visualization"
-       "Some text about visualizations"
-       (= :visualization context)]
-      ]]))
+       {:title "Visualization"
+        :text "Some text about visualizations"
+        :active? (= :visualization context)
+        :href (when ?visualization
+                (format "#/workbooks/%s/questions/%s/visualizations/%s"
+                        (:id ?workbook)
+                        (:id ?question)
+                        (:id ?visualization)))}]]]))
 
 (defn top-bar-links
   []
