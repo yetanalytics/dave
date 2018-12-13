2d89aa5db66681cafa01218f23a5be1d1fc2e6e3
Milt Reder <milt@yetanalytics.com>
2018-12-13 18:42:36 -0500

    select for function arg enums

--- src/com/yetanalytics/dave/ui/views/workbook/question/func.cljs
+++ src/com/yetanalytics/dave/ui/views/workbook/question/func.cljs
@@ -1,5 +1,29 @@
 (ns com.yetanalytics.dave.ui.views.workbook.question.func
-  (:require [re-frame.core :refer [dispatch subscribe]]))
+  (:require [re-frame.core :refer [dispatch subscribe]]
+            [com.yetanalytics.dave.ui.views.form.select
+             :as select]))
+
+
+(defn args [workbook-id question-id]
+  (into [:div.args]
+        (for [[k enum] @(subscribe [:workbook.question.function.func/args-enum
+                                    workbook-id question-id])]
+          [select/select
+           :label (name k)
+           :options (into []
+                          (for [v enum]
+                            {:label (name v)
+                             :value (name v)}))
+           :selected
+           @(subscribe [:workbook.question.function/arg
+                        workbook-id question-id k])
+           :handler #(-> %
+                         keyword
+                         (->> (conj [:workbook.question.function/set-arg!
+                                     workbook-id question-id
+                                     k]))
+                         dispatch)])))
+
 
 (defn info [?workbook-id ?question-id]
   [:div.function
@@ -10,6 +34,14 @@
    [:p.doc
     @(subscribe [:workbook.question.function.func/doc
                  ?workbook-id ?question-id])]
+
+   #_(str @(subscribe [:workbook.question.function/result
+                     ?workbook-id
+                       ?question-id]))
+   #_(str @(subscribe [:workbook.question.function/func
+                     ?workbook-id
+                     ?question-id]))
+   [args ?workbook-id ?question-id]
    [:div (str @(subscribe [:workbook.question.function.result/count
                            ?workbook-id
                            ?question-id])
