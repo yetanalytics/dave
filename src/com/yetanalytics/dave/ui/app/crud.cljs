e3a5884187c08ea36a09ada0b1725b4c6a87e9c7
Milt Reder <milt@yetanalytics.com>
2018-12-14 14:14:11 -0500

    added ns for CRUD stuff

---
+++ src/com/yetanalytics/dave/ui/app/crud.cljs
@@ -0,0 +1,40 @@
+(ns com.yetanalytics.dave.ui.app.crud
+  "CRUD operations for the DAVE object hierarchy"
+  (:require [re-frame.core :as re-frame]))
+
+(re-frame/reg-event-fx
+ :crud/delete-confirm
+ (fn [{:keys [db] :as ctx} [_ & delete-args :as wat]]
+   {:notify/snackbar
+    {:message "Are you sure?"
+     :actionText "Delete"
+     :actionHandler (into [::delete!] delete-args)
+     }}))
+
+(defn- re-index
+  "Re index a map with indexed values"
+  [m]
+  (into {}
+        (map-indexed (fn [idx [k v]]
+                       [k (assoc v :index idx)])
+                     (sort-by (comp :index val)
+                              m))))
+
+(re-frame/reg-event-fx
+ ::delete!
+ (fn [{:keys [db] :as ctx} [_ & object-path]]
+   (let [db-path (interleave [:workbooks :questions :visualizations]
+                             object-path)
+         parent-map-path (butlast db-path)
+         new-db (-> db
+                    (update-in parent-map-path
+                               dissoc
+                               (last db-path))
+                    (update-in parent-map-path
+                               re-index))
+         parent-path (butlast parent-map-path)]
+     {:db new-db
+      :db/save! new-db
+      :notify/snackbar
+      {:message "Deleted!"}
+      :com.yetanalytics.dave.ui.app.nav/nav-path! parent-path})))
