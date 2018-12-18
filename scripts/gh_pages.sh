deba1d718707ec62d8bec5ae870a7184383f31c4
Milt Reder <milt@yetanalytics.com>
2018-12-18 18:31:31 -0500

    add sass compilation to gh_pages script

--- scripts/gh_pages.sh
+++ scripts/gh_pages.sh
@@ -11,8 +11,10 @@ mkdir -p gh_pages/cljs-out
 # copy in the pub dir
 cp -rv resources/public/* gh_pages/
 
-# remove the dev index and replace it with prod
-rm gh_pages/index.html gh_pages/test.html
+# remove the dev/test index, and any css/maps
+rm gh_pages/index.html gh_pages/test.html gh_pages/css/style.css*
+
+# make the prod index the only one
 mv gh_pages/prod_index.html gh_pages/index.html
 
 # build the js
@@ -20,6 +22,12 @@ clojure -A:build-prod
 
 # move it in to the GH pages package
 cp target/public/cljs-out/prod-main.js gh_pages/cljs-out/
+
+# build the css (node_modules must be there for mdc)
+clojure -A:build-sass
+
+# move it
+cp resources/public/css/style.css gh_pages/css/
 
 # Done!
 exit 0
