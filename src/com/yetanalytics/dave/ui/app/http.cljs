e4ef5bac901ec21550d0294ea21ed0fe6a1fab79
Milt Reder <milt@yetanalytics.com>
2019-02-20 10:04:57 -0500

    properly invoke error-handler if present

--- src/com/yetanalytics/dave/ui/app/http.cljs
+++ src/com/yetanalytics/dave/ui/app/http.cljs
@@ -20,13 +20,24 @@
    (a/go (with-redefs [cljs-http.util/json-decode
                        json-decode-str-keys]
            (try (re-frame/dispatch
-                 (conj handler
-                       (a/<! (client/request request))))
+                 (let [{:keys [success
+                               status] :as resp} (a/<! (client/request request))]
+                   ;; single-callback invocation
+                   (if (or success (nil? error-handler))
+                     (conj handler resp)
+                     (conj error-handler
+                           (ex-info "HTTP Error Response"
+                                    {:type ::error-response
+                                     :request request
+                                     :response resp
+                                     :handler handler
+                                     :error-handler error-handler})))))
                 (catch js/Error e
-                  (let [exi (ex-info "HTTP Error"
-                                     {:type ::error
+                  (let [exi (ex-info "Client Error"
+                                     {:type ::client-error
                                       :request request
-                                      :handler handler}
+                                      :handler handler
+                                      :error-handler error-handler}
                                      e)]
                     (if error-handler
                       (re-frame/dispatch (conj error-handler
