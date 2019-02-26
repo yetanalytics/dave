b766b2b2f8e2e547bd8565d3ee8d4edbb5f74242
Milt Reder <milt@yetanalytics.com>
2019-02-26 14:35:37 -0500

    improved select form, with short and fullwidth variants

--- src/com/yetanalytics/dave/ui/views/form/select.cljs
+++ src/com/yetanalytics/dave/ui/views/form/select.cljs
@@ -1,6 +1,5 @@
 (ns com.yetanalytics.dave.ui.views.form.select
-  (:require ["@material/select" :refer [MDCSelect]]
-            ))
+  (:require ["@material/select" :refer [MDCSelect]]))
 
 (defn select
   [& {:keys [label
@@ -8,25 +7,35 @@
              options ;; ordered list of {:value <> :label <>}
 
              handler ;; handler callback
+             full-width?
              ]
-      :or {selected ""}}]
-  [:div.mdc-select
-   {:ref (fn [el]
-           (when el
-             (MDCSelect. el)))
-    }
-   [:i.mdc-select__dropdown-icon]
-   (into [:select.mdc-select__native-control
-          {:value selected
-           :on-change #(-> % .-target .-value handler)}
-          [:option
-           {:value ""
-            :disabled true}]]
-         (for [{:keys [label
-                       value] :as option} options]
-           ^{:key (str "select-option-" value)}
-           [:option
-            {:value value}
-            label]))
-   [:label.mdc-floating-label label]
-   [:div.mdc-line-ripple]])
+      :or {selected ""
+           full-width? false}}]
+  [:div {:class (str "mdc-select dave-select "
+                     (if full-width?
+                       "dave-select-full-width"
+                       "dave-select-width"))
+         :ref (fn [el]
+                (when el
+                  (MDCSelect. el)))}
+   [:input {:type "hidden",
+            :name "enhanced-select"
+            :value selected
+            :on-change #(-> % .-target .-value handler)}]
+   [:i {:class "mdc-select__dropdown-icon"}]
+   [:div {:class "mdc-select__selected-text"}]
+   [:div {:class (str "mdc-select__menu mdc-menu mdc-menu-surface "
+                      (if full-width?
+                        "dave-select-full-width"
+                        "dave-select-width"))}
+    (into [:ul.mdc-list]
+          (for [{:keys [label value]} options]
+            [:li.mdc-list-item
+             (cond-> {:class (when (= selected value)
+                               "mdc-list-item--selected")
+                      :data-value value}
+               (= selected value)
+               (assoc :aria-selected "true"))
+             label]))]
+   [:span {:class "mdc-floating-label"} label]
+   [:div {:class "mdc-line-ripple"}]])
